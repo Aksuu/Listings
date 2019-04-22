16e559086dc8e9bdbc77ad3781d8990a3d0a4e7f
Aksu <appsu-@hotmail.com>
2019-04-22 05:26:25 +0300

    Fixing

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,43 +1,24 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
+const root = '/api';
 
 class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {lists: []};
+		this.state = {items: []};
 	}
 
 	componentDidMount() {
 		client({method: 'GET', path: '/api/Listings'}).done(response => {
-			this.setState({lists: response.entity._embedded.listings});
+			this.setState({items: response.entity._embedded.items});
 		});
 	}
 
 	render() {
 		return (
-			<ListingsList lists={this.state.lists}/>
-		)
-	}
-}
-
-class ListingsList extends React.Component{
-	render() {
-		const lists = this.props.lists.map(employee =>
-			<Listings key={listings._links.self.href} listings={listings}/>
-		);
-		return (
-			<table>
-				<tbody>
-					<tr>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Description</th>
-					</tr>
-					{lists}
-				</tbody>
-			</table>
+			<ItemsList items={this.state.items}/>
 		)
 	}
 }
